9fb7a4c28681e777c4c4a7c5108b76749e4674ad
David Wendt <dcrkid@yahoo.com>
2014-03-16 12:48:21 -0400

    Actually do something across libraries to test that our builds dynamically link properly

--- frontends/appkit/src/main.m
+++ frontends/appkit/src/main.m
@@ -1,7 +1,11 @@
 #import <Cocoa/Cocoa.h>
+#import <icCanvasManagerObjc.h>
 #import <icCanvasAppKit.h>
 
 int main (int argc, char* argv[]) {
+    ICMBrushStroke* bsptr = [[ICMBrushStroke alloc] init];
+    [bsptr penBeginWithX:32 andY:16];
+    
     [NSApplication sharedApplication];
     
     ICAKAppDelegate* theDelegate = [[ICAKAppDelegate alloc] init];
