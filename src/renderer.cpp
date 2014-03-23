50ca987618ba086b259eb30de38ed701ce7c3e77
David Wendt <dcrkid@yahoo.com>
2014-03-23 09:44:38 -0400

    grumble grumble VTABLES

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1,5 +1,7 @@
 #include <icCanvasManager.hpp>
 
+virtual icCanvasManager::Renderer::~Renderer() {};
+
 void icCanvasManager::Renderer::coordToTilespace(const int32_t x, const int32_t y, int32_t* out_tx, int32_t* out_ty) {
     if (out_tx) *out_tx = (int)((float)(x - this->xmin) * this->xscale);
     if (out_ty) *out_ty = (int)((float)(y - this->ymin) * this->yscale);
