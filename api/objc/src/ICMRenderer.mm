c14ede7491410c235f74125bbf9fa73c0b29de40
David Wendt <dcrkid@yahoo.com>
2014-03-23 10:02:43 -0400

    grumble grumble COPY PASTE

--- api/objc/src/ICMRenderer.mm
+++ api/objc/src/ICMRenderer.mm
@@ -17,7 +17,6 @@
 
 - (void)dealloc {
     delete self->_wrapped;
-    [super dealloc];
 };
 
 - (void)enterSurfaceAtX:(const int32_t)x andY:(const int32_t)y withZoom:(const int32_t)zoom andSurface:(cairo_surface_t*)xrsurf withHeight:(const int)height andWidth:(const int)width {
