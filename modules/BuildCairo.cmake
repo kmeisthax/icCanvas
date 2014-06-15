16f0f82030fbe4e8cae4232728f1209941adb145
David Wendt <dcrkid@yahoo.com>
2014-06-15 14:43:55 -0400

    Configure buildscripts to download/build cairo ourselves

--- modules/BuildCairo.cmake
+++ modules/BuildCairo.cmake
@@ -1,11 +1,13 @@
 include(ExternalProject)
 ExternalProject_Add(Pixman
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/ext/pixman
     URL http://cairographics.org/releases/pixman-0.32.4.tar.gz
     CONFIGURE_COMMAND configure
     BUILD_COMMAND make)
 
 ExternalProject_Add(Cairo
     DEPENDS Pixman
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/ext/cairo
     URL http://cairographics.org/releases/cairo-1.12.16.tar.xz
     CONFIGURE_COMMAND configure
     BUILD_COMMAND make)
