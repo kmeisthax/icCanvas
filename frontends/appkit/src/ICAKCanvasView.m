8568a5adf4c18ca08005d05747fd96e677c91612
David Wendt <dcrkid@yahoo.com>
2014-07-07 21:51:17 -0400

    Adjust for compilation errors

--- frontends/appkit/src/ICAKCanvasView.m
+++ frontends/appkit/src/ICAKCanvasView.m
@@ -2,29 +2,30 @@
 #import <icCanvasAppKit.h>
 
 #include <cairo.h>
+#include <cairo-quartz.h>
 
 @implementation ICAKCanvasView {
     ICMRenderer* renderer;
     ICMDrawing* drawing;
 }
 
-- (id)initWithDrawing:(ICMDrawing*) drawing {
+- (id)initWithDrawing:(ICMDrawing*) theDrawing {
     self = [super init];
     
     if (self != nil) {
         self->renderer = [[ICMRenderer alloc] init];
-        self->drawing = drawing;
+        self->drawing = theDrawing;
     }
     
     return self;
 };
 
-- (id)initWithFrame:(NSRect)frameRect andDrawing:(ICMDrawing*) drawing {
+- (id)initWithFrame:(NSRect)frameRect andDrawing:(ICMDrawing*) theDrawing {
     self = [super initWithFrame:frameRect];
     
     if (self != nil) {
         self->renderer = [[ICMRenderer alloc] init];
-        self->drawing = drawing;
+        self->drawing = theDrawing;
     }
     
     return self;
@@ -37,9 +38,9 @@
     
     CGContextTranslateCTM(cgContext, 0.0, self.bounds.size.height);
     CGContextScaleCTM(cgContext, 1.0, -1.0);
-    cairo_surface_t* xrsurf = cairo_quartz_surface_create_for_cg_context(cgContext, self.bounds.size.width, self.bounds.size.height);
+    cairo_surface_t* xrsurf = cairo_quartz_surface_create_for_cg_context(cgContext, (unsigned int)self.bounds.size.width, (unsigned int)self.bounds.size.height);
     
-    [self->renderer enterSurfaceAtX:0 andY:0 withZoom:65535 andSurface:xrsurf withHeight:self.bounds.size.height andWidth:self.bounds.size.width];
+    [self->renderer enterSurfaceAtX:0 andY:0 withZoom:14 andSurface:xrsurf withHeight:self.bounds.size.height andWidth:self.bounds.size.width];
     
     for (int i = 0; i < [self->drawing strokesCount]; i++) {
         [self->renderer drawStroke:[self->drawing strokeAtTime:i]];
