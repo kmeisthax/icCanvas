ed2d71fe59de8aeb66be8368d51edebe470d366e
David Wendt <dcrkid@yahoo.com>
2014-07-11 22:39:53 -0400

    Get everything except the actual algorithm in the class out of the way.

--- src/splinefitter.cpp
+++ src/splinefitter.cpp
@@ -1,13 +1,25 @@
 #include <icCanvasManager.h>
 
-/*
-        std::vector<BrushStoke::__ControlPoint> unfitted_points;
-        BrushStroke::__Spline fitted_curve;
- */
-
 icCanvasManager::SplineFitter::SplineFitter() {};
 icCanvasManager::SplineFitter::~SplineFitter() {};
 
-icCanvasManager::SplineFitter::begin_fitting(icCanvasManager::RefPtr<icCanvasManager::BrushStroke> storage, int error_threshold);
-icCanvasManager::SplineFitter::add_fit_point(int x, int y, int pressure, int tilt, int angle, int dx, int dy);
-icCanvasManager::SplineFitter::finish_fitting();
+icCanvasManager::SplineFitter::begin_fitting(icCanvasManager::RefPtr<icCanvasManager::BrushStroke> storage, int error_threshold) {
+    this->unfitted_points.clear();
+    this->target_curve = storage;
+};
+
+icCanvasManager::SplineFitter::add_fit_point(int x, int y, int pressure, int tilt, int angle, int dx, int dy) {
+    icCanvasManager::BrushStroke::__ControlPoint cp;
+    
+    cp.x = x;
+    cp.y = y;
+    cp.pressure = pressure;
+    cp.tilt = tilt;
+    cp.angle = angle;
+    cp.dx = dx;
+    cp.dy = dy;
+    
+    this->unfitted_points.push_back(cp);
+};
+
+icCanvasManager::SplineFitter::finish_fitting() {};
