1eb2b96110e264129dae016b87113e853e15bd89
David Wendt <dcrkid@yahoo.com>
2014-07-19 14:13:41 -0400

    Add GTK support for CanvasView mouse input.

--- frontends/gtk/include/icCanvasGtk/CanvasWidget.hpp
+++ frontends/gtk/include/icCanvasGtk/CanvasWidget.hpp
@@ -1,19 +1,26 @@
 #ifndef __ICCANVASGTK__CANVASWIDGET_HPP__
 #define __ICCANVASGTK__CANVASWIDGET_HPP__
 
-#include <gtkmm/widget.h>
+#include <gtkmm.h>
 #include <icCanvasManager.hpp>
 
 namespace icCanvasGtk {
     class CanvasWidget : public Gtk::Widget {
             icCanvasManager::RefPtr<icCanvasManager::CanvasView> cv;
+
+            //GDK doesn't provide mouse event deltas, so we have to generate them ourselves
+            double lastx, lasty;
         public:
             CanvasWidget();
             virtual ~CanvasWidget();
             
             void set_drawing(icCanvasManager::RefPtr<icCanvasManager::Drawing> newDoc);
         protected:
-            virtual bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr);
+            virtual bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override;
+
+            virtual bool on_button_press_event(GdkEventButton *evt) override;
+            virtual bool on_motion_notify_event(GdkEventMotion *evt) override;
+            virtual bool on_button_release_event(GdkEventButton *evt) override;
     };
 }
 
--- frontends/gtk/src/CanvasWidget.cpp
+++ frontends/gtk/src/CanvasWidget.cpp
@@ -1,7 +1,8 @@
 #include <icCanvasGtk.hpp>
 
 icCanvasGtk::CanvasWidget::CanvasWidget() :
-    Glib::ObjectBase("icCanvasGtkCanvasWidget"), Gtk::Widget()
+    Glib::ObjectBase("icCanvasGtkCanvasWidget"), Gtk::Widget(),
+    lastx(0), lasty(0)
 {
     this->set_has_window(false);
     this->cv = new icCanvasManager::CanvasView();
@@ -27,5 +28,36 @@ bool icCanvasGtk::CanvasWidget::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
     dirtyRect.height = y2 - y1;
     this->cv->draw(capi_cr, dirtyRect);
     
+    return true;
+}
+
+bool icCanvasGtk::CanvasWidget::on_button_press_event(GdkEventButton *evt) {
+    if (evt->type == GDK_BUTTON_PRESS) {
+        this->cv->mouse_down(evt->x, evt->y, 0, 0);
+
+        this->lastx = evt->x;
+        this->lasty = evt->y;
+    }
+
+    return true;
+}
+
+bool icCanvasGtk::CanvasWidget::on_motion_notify_event(GdkEventMotion *evt) {
+    this->cv->mouse_drag(evt->x, evt->y, evt->x - this->lastx, evt->y - this->lasty);
+
+    this->lastx = evt->x;
+    this->lasty = evt->y;
+
+    return true;
+}
+
+bool icCanvasGtk::CanvasWidget::on_button_release_event(GdkEventButton *evt) {
+    if (evt->type == GDK_BUTTON_RELEASE) {
+        this->cv->mouse_up(evt->x, evt->y, evt->x - this->lastx, evt->y - this->lasty);
+
+        this->lastx = evt->x;
+        this->lasty = evt->y;
+    }
+
     return true;
 }
