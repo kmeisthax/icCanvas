18941744d991fc6dd068b49133564af8e6bd1377
David Wendt <dcrkid@yahoo.com>
2014-07-25 22:35:23 -0400

    CanvasView needs to accomidate for the physical display pixel density

--- include/icCanvasManager/canvasview.hpp
+++ include/icCanvasManager/canvasview.hpp
@@ -19,6 +19,7 @@ namespace icCanvasManager {
         int x_scroll, y_scroll; //The top-left edge of the view in canvas coord
         int x_center, y_center; //The center of the screen, also canvas coords
         float x_scale, y_scale;
+        float ui_scale;
 
         //During spline fitting only.
         RefPtr<BrushStroke> built_stroke;
@@ -34,11 +35,20 @@ namespace icCanvasManager {
 
         /* Respond to a drawing request from the native window system or UI
          * toolkit.
+         *
+         * The provided cairo context must provide a coordinate system scaled
+         * such that one pixel in the context represents as many physical
+         * display pixels as is specified in set_size. Some window systems may
+         * perform this for you, others do not.
          */
         void draw(cairo_t *ctxt, cairo_rectangle_t dirtyArea);
 
-        /* Respond to a resize from the native window system. */
-        void set_size(double width, double height);
+        /* Respond to a resize from the native window system.
+         *
+         * The ui_scale parameter indicates how many physical display pixels
+         * are present per window-space pixels.
+         */
+        void set_size(double width, double height, double ui_scale);
 
         /* Respond to mouse input. */
         void mouse_down(int x, int y, int deltaX, int deltaY);
