ade2d3df35ab66c12d5f5786054c5f1f0ce7a4cb
David Wendt <dcrkid@yahoo.com>
2014-08-18 23:40:15 -0400

    Experimenting with a C-only API binding for reuse in other languages.

---
+++ api/c/include/icCanvasManagerC/icmdrawing.h
@@ -0,0 +1,28 @@
+#ifndef __ICCANVASMANAGER_CAPI__ICM_DRAWING__H__
+#define __ICCANVASMANAGER_CAPI__ICM_DRAWING__H__
+
+#include <icCanvasManagerC.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    struct icm_drawing {
+        void* __wrapped;
+    }
+
+    icm_drawing* icm_drawing_construct();
+    int icm_drawing_reference(icm_drawing *this);
+    int icm_drawing_dereference(icm_drawing *this);
+
+    icm_brushstroke* icm_drawing_stroke_at_time(icm_drawing *this, int time);
+    int icm_drawing_strokes_count(icm_drawing *this);
+
+    void icm_drawing_append_stroke(icm_drawing *this, icm_brushstroke *stroke);
+    icm_tilecache* icm_drawing_get_tilecache(icm_drawing *this);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
