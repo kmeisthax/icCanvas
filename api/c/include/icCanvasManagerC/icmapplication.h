4e5779f96431e65d6a9457ca4ec34423901095ab
David Wendt <dcrkid@yahoo.com>
2014-08-23 15:16:51 -0400

    Add bogus ref/deref C API functions for use by Vala and other languages which require one of these

--- api/c/include/icCanvasManagerC/icmapplication.h
+++ api/c/include/icCanvasManagerC/icmapplication.h
@@ -11,6 +11,9 @@ extern "C" {
 
     icm_application* icm_application_get_instance();
 
+    int icm_application_reference(icm_application *this);
+    int icm_application_dereference(icm_application *this);
+
     void icm_application_background_tick(icm_application *this);
     icm_renderscheduler* icm_application_get_render_scheduler(icm_application *this);
 
