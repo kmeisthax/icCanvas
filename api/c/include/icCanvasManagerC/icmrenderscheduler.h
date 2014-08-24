0c81248d81c8589d96786a053f45e986dbe362c1
David Wendt <dcrkid@yahoo.com>
2014-08-23 21:01:42 -0400

    Not sure how the hell I forgot those parameters

--- api/c/include/icCanvasManagerC/icmrenderscheduler.h
+++ api/c/include/icCanvasManagerC/icmrenderscheduler.h
@@ -13,10 +13,10 @@ extern "C" {
     int icm_renderscheduler_reference(icm_renderscheduler *this);
     int icm_renderscheduler_dereference(icm_renderscheduler *this);
 
-    void icm_renderscheduler_request_tile(icm_drawing *d, int x, int y, int size, int time);
-    void icm_renderscheduler_revoke_request(icm_drawing *d, int x_min, int y_min, int x_max, int y_max);
-    void icm_renderscheduler_background_tick();
-    int icm_renderscheduler_collect_requests(icm_drawing *d);
+    void icm_renderscheduler_request_tile(icm_renderscheduler *this, icm_drawing *d, int x, int y, int size, int time);
+    void icm_renderscheduler_revoke_request(icm_renderscheduler *this, icm_drawing *d, int x_min, int y_min, int x_max, int y_max);
+    void icm_renderscheduler_background_tick(icm_renderscheduler *this);
+    int icm_renderscheduler_collect_requests(icm_renderscheduler *this, icm_drawing *d);
 
 #ifdef __cplusplus
 }
--- api/c/src/icmrenderscheduler.cpp
+++ api/c/src/icmrenderscheduler.cpp
@@ -26,22 +26,22 @@ extern "C" {
         return refcount;
     };
 
-    void icm_renderscheduler_request_tile(icm_drawing *d, int x, int y, int size, int time) {
+    void icm_renderscheduler_request_tile(icm_renderscheduler *w, icm_drawing *d, int x, int y, int size, int time) {
         icCanvasManager::RenderScheduler* d = (icCanvasManager::RenderScheduler*)w;
         d->request_tile(x, y, size, time);
     };
 
-    void icm_renderscheduler_revoke_request(icm_drawing *d, int x_min, int y_min, int x_max, int y_max) {
+    void icm_renderscheduler_revoke_request(icm_renderscheduler *w, icm_drawing *d, int x_min, int y_min, int x_max, int y_max) {
         icCanvasManager::RenderScheduler* d = (icCanvasManager::RenderScheduler*)w;
         d->revoke_request(x_min, y_min, x_max, y_max);
     };
 
-    void icm_renderscheduler_background_tick() {
+    void icm_renderscheduler_background_tick(icm_renderscheduler *w) {
         icCanvasManager::RenderScheduler* d = (icCanvasManager::RenderScheduler*)w;
         d->background_tick();
     };
 
-    int icm_renderscheduler_collect_requests(icm_drawing *d) {
+    int icm_renderscheduler_collect_requests(icm_renderscheduler *w, icm_drawing *d) {
         icCanvasManager::RenderScheduler* d = (icCanvasManager::RenderScheduler*)w;
         return d->collect_requests();
     };
