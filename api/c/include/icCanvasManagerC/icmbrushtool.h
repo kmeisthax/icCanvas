100c2afa72c7ba45a5e78672587086dbb9dbadeb
David Wendt <dcrkid@yahoo.com>
2014-11-27 23:02:28 -0500

    Finish the BrushTool wrapper and fix some errors in the CanvasTool wrapper.

---
+++ api/c/include/icCanvasManagerC/icmbrushtool.h
@@ -0,0 +1,57 @@
+#ifndef __ICCANVASMANAGER_CAPI__ICM_BRUSHTOOL__H__
+#define __ICCANVASMANAGER_CAPI__ICM_BRUSHTOOL__H__
+
+#include <icCanvasManagerC.h>
+
+#ifdef __cplusplus
+extern "C" {
+#else
+#include <stdbool.h>
+#endif
+
+    typedef void *icm_brushtool;
+
+    icm_drawing icm_brushtool_construct();
+    int icm_brushtool_reference(icm_brushtool wrap);
+    int icm_brushtool_dereference(icm_brushtool wrap);
+
+    /*Downcast and upcast functions for converting between icm_canvastool and
+      this class. The downcast function may return NULL if the object passed in
+      is not an instance of this class. */
+    icm_brushtool icm_brushtool_downcast(icm_canvastool up_obj);
+    icm_canvastool icm_brushtool_upcast(icm_brushtool down_obj);
+
+    /*Delegates are used to capture brush strokes.
+      They may exist on the C++ side or be created by bindings (see below)
+
+      Note that it is technically possible to build a non-reference-counted
+      delegate. If this is the case then ref/deref are no-ops and deref returns
+      negative 1. */
+    typedef void *icm_brushtool_delegate;
+
+    int icm_brushtool_delegate_reference(icm_brushtool_delegate wrap);
+    int icm_brushtool_delegate_dereference(icm_brushtool_delegate wrap);
+
+    /*Custom delegates are used to allow C code to be called from C++ code.*/
+    icm_brushtool_delegate icm_brushtool_delegate_construct_custom();
+    bool icm_brushtool_delegate_is_custom(icm_brushtool_delegate wrap);
+
+    /*These functions set the function pointers of delegates.
+     *Do NOT call them on delegates that return FALSE from is_custom, or your
+     *program will crash.
+     *
+     *The user_data ptr will be passed on invocation of your delegate. Use it
+     *to store necessary state data. The lifetime of the pointed-to data must
+     *extend beyond the life of the custom delegate.
+     */
+    typedef void (*icm_brushtool_delegate_captured_stroke_func) (void* user_data, icm_brushstroke stroke);
+    void icm_brushtool_delegate_set_custom_captured_stroke_func(icm_brushtool_delegate w, icm_brushtool_delegate_captured_stroke_func func, void* user_data);
+
+    /*Finally, you can set your delegate here.*/
+    void icm_brushtool_set_delegate(icm_brushtool wrap, icm_brushtool_delegate del);
+    icm_brushtool_delegate icm_brushtool_get_delegate(icm_brushtool wrap);
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ api/c/src/icmbrushtool.cpp
@@ -0,0 +1,114 @@
+#include <icCanvasManager.hpp>
+
+#include <icCanvasManagerC.h>
+
+namespace icCanvasManager {
+    class BrushToolDelegateCImpl : public BrushTool::Delegate, public RefCnt {
+        icm_brushtool_delegate_captured_stroke_func captured_stroke_impl;
+        void* captured_stroke_user_data;
+
+    public:
+        BrushToolDelegateCImpl() : captured_stroke_impl(NULL), captured_stroke_user_data(NULL) {}
+        virtual ~BrushToolDelegateCImpl() {}
+
+        virtual void captured_stroke(RefPtr<BrushStroke> stroke) {
+            if (this->captured_stroke_impl) {
+                BrushStroke* bs = stroke;
+                this->captured_stroke_impl(this->captured_stroke_user_data, (void*)bs);
+            }
+        }
+
+        friend void ::icm_brushtool_delegate_set_custom_captured_stroke_func(icm_brushtool_delegate, icm_brushtool_delegate_captured_stroke_func, void*);
+    };
+}
+
+extern "C" {
+    icm_brushtool icm_brushtool_construct() {
+        icCanvasManager::BrushTool* d = new icCanvasManager::BrushTool();
+        d->ref();
+
+        return (icm_brushtool)d;
+    };
+
+    int icm_brushtool_reference(icm_brushtool w) {
+        icCanvasManager::BrushTool* d = (icCanvasManager::BrushTool*)w;
+        return d->ref();
+    };
+
+    int icm_brushtool_dereference(icm_brushtool w) {
+        icCanvasManager::BrushTool* d = (icCanvasManager::BrushTool*)w;
+        int refcount = d->deref();
+
+        if (refcount <= 0) {
+            delete d;
+        }
+
+        return refcount;
+    };
+
+    icm_brushtool icm_brushtool_downcast(icm_canvastool up_obj) {
+        icCanvasManager::CanvasTool* up = (icCanvasManager::CanvasTool*)up_obj;
+        icCanvasManager::BrushTool* down = dynamic_cast<icCanvasManager::BrushTool*>(up);
+
+        return (void*)down;
+    };
+
+    icm_canvastool icm_brushtool_upcast(icm_brushtool down_obj) {
+        icCanvasManager::BrushTool* down = (icCanvasManager::BrushTool*)down_obj;
+        icCanvasManager::CanvasTool* up = static_cast<icCanvasManager::CanvasTool*>(down);
+
+        return (void*)up;
+    };
+
+    int icm_brushtool_delegate_reference(icm_brushtool_delegate w) {
+        icCanvasManager::BrushTool::Delegate* d = (icCanvasManager::BrushTool::Delegate*)w;
+        icCanvasManager::RefCnt* d_refable = dynamic_cast<icCanvasManager::RefCnt*>(d_refable);
+
+        if (d_refable) {
+            return d_refable->ref();
+        } else {
+            return -1;
+        }
+    };
+
+    int icm_brushtool_delegate_dereference(icm_brushtool_delegate w) {
+        icCanvasManager::BrushTool::Delegate* d = (icCanvasManager::BrushTool::Delegate*)w;
+        icCanvasManager::RefCnt* d_refable = dynamic_cast<icCanvasManager::RefCnt*>(d_refable);
+
+        if (d_refable) {
+            int refcount = d_refable->deref();
+
+            if (refcount <= 0) {
+                delete d;
+            }
+
+            return refcount;
+        } else {
+            return -1;
+        }
+    };
+
+    icm_brushtool_delegate icm_brushtool_delegate_construct_custom() {
+        auto* dcustom = new icCanvasManager::BrushToolDelegateCImpl();
+        auto* d = static_cast<icCanvasManager::BrushTool::Delegate*>(dcustom);
+
+        return (void*)d;
+    };
+
+    bool icm_brushtool_delegate_is_custom(icm_brushtool_delegate w) {
+        icCanvasManager::BrushTool::Delegate* d = (icCanvasManager::BrushTool::Delegate*)w;
+        auto* dcustom = dynamic_cast<icCanvasManager::BrushToolDelegateCImpl*>(d);
+
+        return dcustom != NULL;
+    };
+
+    void icm_brushtool_delegate_set_custom_captured_stroke_func(icm_brushtool_delegate w, icm_brushtool_delegate_captured_stroke_func func, void* user_data) {
+        icCanvasManager::BrushTool::Delegate* d = (icCanvasManager::BrushTool::Delegate*)w;
+        auto* dcustom = dynamic_cast<icCanvasManager::BrushToolDelegateCImpl*>(d);
+
+        if (dcustom) {
+            dcustom->captured_stroke_impl = func;
+            dcustom->captured_stroke_user_data = user_data;
+        }
+    };
+}
