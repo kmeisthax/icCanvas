3830bea7e037e048091b09f82a2cd385bfa50397
David Wendt <dcrkid@yahoo.com>
2015-01-06 21:14:41 -0500

    Move the build system for linux-gtk into a sub-directory, to make way for a windows-gtk port

--- linux-gtk-debug-configure.sh
+++ linux-gtk-debug-configure.sh
@@ -4,4 +4,4 @@ mkdir build
 mkdir build/linux-gtk-debug
 cd build/linux-gtk-debug
 
-cmake -DCMAKE_BUILD_TYPE=Debug ../../frontends/gtk
+cmake -DCMAKE_BUILD_TYPE=Debug ../../frontends/gtk/platform/gnulinux-desktop
