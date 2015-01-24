70dfdfffe4d588153fc8ab9bd301db5d908170ff
David Wendt <dcrkid@yahoo.com>
2015-01-24 14:10:25 -0500

    Walk back our original plans of multiple build systems per frontend.

--- linux-gtk-debug-configure.sh
+++ linux-gtk-debug-configure.sh
@@ -4,4 +4,4 @@ mkdir build
 mkdir build/linux-gtk-debug
 cd build/linux-gtk-debug
 
-cmake -DCMAKE_BUILD_TYPE=Debug ../../frontends/gtk/platform/gnulinux-desktop
+cmake -DCMAKE_BUILD_TYPE=Debug ../../frontends/gtk/
