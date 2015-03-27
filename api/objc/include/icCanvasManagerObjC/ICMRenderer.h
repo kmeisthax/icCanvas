79e470a6430eafa2742fea76c70c10069184f9f8
David Wendt <dcrkid@yahoo.com>
2015-03-27 17:32:30 -0400

    Remove bare init method as ICMRenderer is now an abstract class.

--- api/objc/include/icCanvasManagerObjC/ICMRenderer.h
+++ api/objc/include/icCanvasManagerObjC/ICMRenderer.h
@@ -4,7 +4,6 @@
 
 @interface ICMRenderer : NSObject
 
-- (id)init;
 - (id)initFromWrappedObject:(void*)optr;
 
 - (void)enterSurfaceAtX:(const int32_t)x andY:(const int32_t)y withZoom:(const int32_t)zoom andSurface:(cairo_surface_t*)xrsurf withHeight:(const int)height andWidth:(const int)width;
--- api/objc/src/ICMRenderer.mm
+++ api/objc/src/ICMRenderer.mm
@@ -5,16 +5,6 @@
     icCanvasManager::RefPtr<icCanvasManager::Renderer> _wrapped;
 }
 
-- (id)init {
-    self = [super init];
-    
-    if (self != nil) {
-        self->_wrapped = new icCanvasManager::Renderer();
-    }
-    
-    return self;
-};
-
 - (id)initFromWrappedObject:(void*)optr {
     self = [super init];
     
