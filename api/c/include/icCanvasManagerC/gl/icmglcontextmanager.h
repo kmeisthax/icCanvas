15f7350ef19417bab6f705daa4fa27c43655af0b
David Wendt <dcrkid@yahoo.com>
2015-04-06 19:47:10 -0400

    C syntax regarding function pointers is bollocks

--- api/c/include/icCanvasManagerC/gl/icmglcontextmanager.h
+++ api/c/include/icCanvasManagerC/gl/icmglcontextmanager.h
@@ -19,7 +19,7 @@ extern "C" {
     typedef void (*icm_gl_shutdown_sub_context_func)(intptr_t ctxt, void* user_data);
     typedef intptr_t (*icm_gl_make_current_func)(intptr_t ctxt, intptr_t drawable, void* user_data);
     typedef intptr_t (*icm_gl_get_current_func)(void* user_data);
-    typedef void(*)() (*icm_gl_get_proc_address)(char* proc_name, void* user_data);
+    typedef void(*(*icm_gl_get_proc_address)()) (char* proc_name, void* user_data);
 
     typedef struct {
         icm_gl_create_main_context_func create_main_context;
