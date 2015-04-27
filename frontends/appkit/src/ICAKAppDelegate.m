3f595675065d168687015d762048cf9dfeecc9a3
David Wendt <dcrkid@yahoo.com>
2015-04-27 19:43:41 -0400

    Actually wire up the GL Renderer to the point where we can make rendering commands.
    (I had this earlier, but I didn't wanna commit it until the fragment shaders compiled at least.)

--- frontends/appkit/src/ICAKAppDelegate.m
+++ frontends/appkit/src/ICAKAppDelegate.m
@@ -13,6 +13,12 @@
     
     ICAKDockingController* _dock_ctrl;
     ICAKToolPaletteController* _tpal_ctrl;
+
+    ICMGLRenderer* _gl_render;
+    ICAKGLContextManager* _gl_ctxts;
+
+    NSView* _osView;
+    NSWindow* _wnd;
 }
 
 - (id)init {
@@ -23,6 +29,16 @@
         self->_dock_ctrl = [[ICAKDockingController alloc] init];
         self->_tpal_ctrl = [[ICAKToolPaletteController alloc] init];
         
+        //Create a hidden window to bind NSOpenGLContext to.
+        self->_wnd = [[NSWindow alloc] init];
+        self->_osView = [[NSView alloc] init];
+        self->_wnd.contentView = self->_osView;
+
+        self->_gl_ctxts = [[ICAKGLContextManager alloc] init];
+
+        intptr_t ctxt = [self->_gl_ctxts createMainContextWithVersionMajor:3 andMinor:2];
+        self->_gl_render = [[ICMGLRenderer alloc] initWithContextManager:self->_gl_ctxts andContext:ctxt andDrawable:(intptr_t)(__bridge void*)self->_osView];
+
         self->_tpal_ctrl.dockingController = self->_dock_ctrl;
         self->_active_task_timer = nil;
     }
